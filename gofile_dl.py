ff4d47f1c89f1d4ee21238ae901060a4466d46a7
Yuuki <82380343+xcscxr@users.noreply.github.com>
2022-02-08 15:14:40 +0530

    --

--- gofile_dl.py
+++ gofile_dl.py
@@ -26,3 +26,29 @@ def gofile_dl(url):
 # =================================
 
 print(gofile_dl(url))
+
+# =================================
+
+'''
+SAMPLE OUTPUT:
+ - List of file objects
+[
+    {
+        'id': 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX', 
+        'type': 'file', 
+        'name': 'XXX', 
+        'parentFolder': 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX', 
+        'createTime': 1837708755, 
+        'size': 11851111419,
+        'downloadCount': 442, 
+        'md5': '509d4472ea7e00000000000000008bd3', 
+        'mimetype': 'application/vnd.rar', 
+        'serverChoosen': 'store1', 
+        'directLink': 'https://store1.gofile.io/download/XYZ', 
+        'link': 'https://store1.gofile.io/download/XYZ'
+    },
+    {
+        # more objects for multi-files
+    }
+]
+'''
