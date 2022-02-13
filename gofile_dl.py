6c3d46e4c6e56ba76b075b63fb9e4cb5d356d407
Yuuki <82380343+xcscxr@users.noreply.github.com>
2022-02-13 17:53:10 +0530

    Include accountToken in output

--- gofile_dl.py
+++ gofile_dl.py
@@ -1,6 +1,6 @@
 import requests
 
-url = ''
+url = 'https://gofile.io/d/j9lj35'
 
 # =================================
 
@@ -8,7 +8,7 @@ def gofile_dl(url):
     api_uri = 'https://api.gofile.io'
     client = requests.Session()
     res = client.get(api_uri+'/createAccount').json()
-
+    
     data = {
         'contentId': url.split('/')[-1],
         'token': res['data']['token'],
@@ -20,8 +20,11 @@ def gofile_dl(url):
     content = []
     for item in res['data']['contents'].values():
         content.append(item)
-        
-    return content
+    
+    return {
+        'accountToken': data['token'],
+        'files': content
+    }
 
 # =================================
 
@@ -31,24 +34,26 @@ print(gofile_dl(url))
 
 '''
 SAMPLE OUTPUT:
- - List of file objects
-[
-    {
-        'id': 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX', 
-        'type': 'file', 
-        'name': 'XXX', 
-        'parentFolder': 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX', 
-        'createTime': 1837708755, 
-        'size': 11851111419,
-        'downloadCount': 442, 
-        'md5': '509d4472ea7e00000000000000008bd3', 
-        'mimetype': 'application/vnd.rar', 
-        'serverChoosen': 'store1', 
-        'directLink': 'https://store1.gofile.io/download/XYZ', 
-        'link': 'https://store1.gofile.io/download/XYZ'
-    },
-    {
-        # more objects for multi-files
-    }
-]
+{
+    accountToken: 'xxxxx' (to be used as a cookie if ddl doesn't work)
+    files: [
+        {
+            'id': 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX', 
+            'type': 'file', 
+            'name': 'XXX', 
+            'parentFolder': 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX', 
+            'createTime': 1837708755, 
+            'size': 11851111419,
+            'downloadCount': 442, 
+            'md5': '509d4472ea7e00000000000000008bd3', 
+            'mimetype': 'application/vnd.rar', 
+            'serverChoosen': 'store1', 
+            'directLink': 'https://store1.gofile.io/download/XYZ', 
+            'link': 'https://store1.gofile.io/download/XYZ'
+        },
+        {
+            # more objects for multi-files
+        }
+    ]
+}
 '''
